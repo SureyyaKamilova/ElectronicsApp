85ba6f5a0a0005e1734694682299c40a8793f44d
SureyyaKamilova <sureyyakamilovaa@gmail.com>
2023-06-29 21:46:08 +0400

    Add project files.

---
+++ ElectronicsApp/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using ElectronicsApp.ViewModels.Windows;
+using ElectronicsApp.Views.Windows;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ElectronicsApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            DashBoard dashBoard = new DashBoard();
+            DashBoardViewModel dashBoardViewModel=new DashBoardViewModel();
+
+            dashBoard.DataContext = dashBoardViewModel;
+
+            MainWindow = dashBoard;
+            MainWindow.Show();
+        }
+        
+    }
+}
